171949904a5762c4273d8c5e34063405f2bc9ad2
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-08-19 01:33:10 +0000

    Resolução Exercício for 2

---
+++ ExercicioRepeticao2.java
@@ -0,0 +1,13 @@
+public class ExercicioRepeticao2{
+    public static void main(String args[]){
+        int soma=0;
+        for(int i=1;i<=1000;i++){
+            soma=soma+i;
+
+        }
+        System.out.println("A soma é:" + soma);
+    }
+
+}
+
+//2. Imprima a soma de 1 até 1000.
